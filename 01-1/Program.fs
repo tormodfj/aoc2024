ed92dab6346cc6843c9640d64e6a2c4bedfbdaa5
Tormod Fjeldskår <tormod@justify.no>
2024-12-05 22:10:25 +0100

    Day 1

---
+++ 01-1/Program.fs
@@ -0,0 +1,14 @@
+﻿open System.IO
+
+let left, right =
+    File.ReadLines "input.txt"
+    |> Seq.map (fun line -> line.Split("   "))
+    |> Seq.map (fun parts -> (int parts.[0], int parts.[1]))
+    |> Seq.fold (fun (xs, ys) (x, y) -> (x::xs, y::ys)) ([],[])
+    
+let difference (x, y) = x - y |> abs
+
+Seq.zip (left |> Seq.sort) (right |> Seq.sort)
+|> Seq.map difference
+|> Seq.sum
+|> printfn "%i"
---
+++ 01-2/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.IO
+
+let left, right =
+    File.ReadLines "input.txt"
+    |> Seq.map (fun line -> line.Split("   "))
+    |> Seq.map (fun parts -> (int parts.[0], int parts.[1]))
+    |> Seq.fold (fun (xs, ys) (x, y) -> (x::xs, y::ys)) ([],[])
+
+let appearanceMap =
+    right
+    |> Seq.countBy id
+    |> Map.ofSeq
+    
+let score x =
+    x
+    |> appearanceMap.TryFind
+    |> Option.defaultValue 0
+    |> (*) x
+
+left
+|> Seq.map score
+|> Seq.sum
+|> printfn "%i"
