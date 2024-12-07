cd46fce4335b23676720ad6fd114eece1f570ac8
Tormod Fjeldskår <tormod.fjeldskaar@gmail.com>
2024-12-07 23:21:17 +0100

    Day 4

---
+++ 04-1/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+open System.IO
+open System.Text.RegularExpressions
+
+let map =
+    File.ReadLines "input.txt"
+    |> array2D
+    
+let getChar (x,y) = try map[x,y] with | _ -> '.'
+
+let N (x, y) = $"{getChar (x,y)}{getChar (x,y-1)}{getChar (x,y-2)}{getChar (x,y-3)}"
+let NE (x, y) = $"{getChar (x,y)}{getChar (x+1,y-1)}{getChar (x+2,y-2)}{getChar (x+3,y-3)}"
+let E (x, y) = $"{getChar (x,y)}{getChar (x+1,y)}{getChar (x+2,y)}{getChar (x+3,y)}"
+let SE (x, y) = $"{getChar (x,y)}{getChar (x+1,y+1)}{getChar (x+2,y+2)}{getChar (x+3,y+3)}"
+let S (x, y) = $"{getChar (x,y)}{getChar (x,y+1)}{getChar (x,y+2)}{getChar (x,y+3)}"
+let SW (x, y) = $"{getChar (x,y)}{getChar (x-1,y+1)}{getChar (x-2,y+2)}{getChar (x-3,y+3)}"
+let W (x, y) = $"{getChar (x,y)}{getChar (x-1,y)}{getChar (x-2,y)}{getChar (x-3,y)}"
+let NW (x, y) = $"{getChar (x,y)}{getChar (x-1,y-1)}{getChar (x-2,y-2)}{getChar (x-3,y-3)}"
+
+let countXmas x y ch =
+    let count (x,y) =
+        [| N(x,y); NE(x,y); E(x,y); SE(x,y); S(x,y); SW(x,y); W(x,y); NW(x,y) |]
+        |> Array.filter (fun input -> input = "XMAS")
+        |> Array.length
+    
+    match ch with
+    | 'X' -> count (x,y)
+    | _ -> 0
+    
+map
+|> Array2D.mapi countXmas
+|> Seq.cast<int>
+|> Seq.sum
+|> printfn "%i"
---
+++ 04-2/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+open System.Diagnostics
+open System.IO
+open System.Text.RegularExpressions
+
+let map =
+    File.ReadLines "input.txt"
+    |> array2D
+    
+let getChar (x,y) = try map[x,y] with | _ -> '.'
+
+let NWSE (x, y) = $"{getChar (x-1,y-1)}{getChar (x,y)}{getChar (x+1,y+1)}"
+let NESW (x, y) = $"{getChar (x+1,y-1)}{getChar (x,y)}{getChar (x-1,y+1)}"
+
+let findXmas x y ch =
+    let isXmas coord =
+        match NWSE(coord), NESW(coord) with
+        | "MAS","MAS" -> true
+        | "MAS","SAM" -> true
+        | "SAM","MAS" -> true
+        | "SAM","SAM" -> true
+        | _ -> false
+
+    match ch with
+    | 'A' when isXmas(x,y) -> 1
+    | _ -> 0
+    
+map
+|> Array2D.mapi findXmas
+|> Seq.cast<int>
+|> Seq.sum
+|> printfn "%i"
